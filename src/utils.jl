ff89db662d9581b26966cbc9864fb7dd8ba195b3
SimonDanisch <sdanisch@gmail.com>
2016-08-07 11:51:48 +0200

    migrate files from GLVisualize

---
+++ src/utils.jl
@@ -0,0 +1,8 @@
+function sumlengths(points)
+    result = zeros(eltype(points[1]), length(points))
+    for i=1:length(points)
+        i0 = max(i-1,1)
+        result[i] = result[i0] + norm(points[i0]-points[i])
+    end
+    result
+end
