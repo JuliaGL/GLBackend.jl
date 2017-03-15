c6eec6de9dfbe1c25bd2fee91ad4ed7f863998b0
SimonDanisch <sdanisch@gmail.com>
2017-03-15 12:40:33 +0100

    whatever

--- src/shader/lines.vert
+++ src/shader/lines.vert
@@ -2,8 +2,16 @@
 {{GLSL_EXTENSIONS}}
 
 {{vertex_type}} position;
-{{color_type}} color;
 in float lastlen;
+
+{{color_type}}      color;
+{{color_map_type}}  color_map;
+{{intensity_type}}  intensity;
+{{color_norm_type}} color_norm;
+
+vec4 _color(vec3 color, Nothing intensity, Nothing color_map, Nothing color_norm, int index);
+vec4 _color(vec4 color, Nothing intensity, Nothing color_map, Nothing color_norm, int index);
+vec4 _color(Nothing color, float intensity, sampler1D color_map, vec2 color_norm, int index);
 
 uniform mat4 projection, view, model;
 uniform uint objectid;
@@ -26,7 +34,7 @@ void main()
     g_lastlen = lastlen;
     int index = gl_VertexID;
     g_id = uvec2(objectid, index+1);
-    g_color = {{color_calculation}};
+    g_color = _color(color, intensity, color_map, color_norm, index);
     g_line_connections = uint(index/dims.x);
     gl_Position = projection*view*model*to_vec4(position);
 }
--- src/texture_atlas.jl
+++ src/texture_atlas.jl
@@ -27,34 +27,71 @@ end
 
 
 begin #basically a singleton for the textureatlas
-const local TEXTURE_ATLAS = TextureAtlas[]
-reset_texture_atlas!() = empty!(TEXTURE_ATLAS)
-function get_texture_atlas()
-    not_initilized = isempty(TEXTURE_ATLAS)
-    if not_initilized
-        fn = assetpath("fonts", "DejaVuSansMono.ttf")
-        global DEFAULT_FONT_FACE = newface(fn)
-        atlas = push!(TEXTURE_ATLAS, TextureAtlas())[] # initialize only on demand
-        for c in '\u0000':'\u00ff' #make sure all ascii is mapped linearly
-            insert_glyph!(atlas, c, DEFAULT_FONT_FACE)
+
+const local _textute_atlas = TextureAtlas[]
+const local _cache_path = joinpath(dirname(@__FILE__), "..", ".cache", "texture_atlas.jls")
+const local _default_font = Vector{Ptr{FreeType.FT_FaceRec}}[]
+
+
+function defaultfont()
+    if isempty(_default_font)
+        push!(_default_font, newface(assetpath("fonts", "DejaVuSansMono.ttf")))
+    end
+    _default_font[]
+end
+
+reset_texture_atlas!() = empty!(_textute_atlas)
+
+function cached_load()
+    if isfile(_cache_path)
+        return open(_cache_path) do io
+            dict = deserialize(io)
+            dict[:images] = Texture(dict[:images]) # upload to GPU
+            fields = [dict[n] for n in fieldnames(TextureAtlas)]
+            TextureAtlas(fields...)
         end
-        return atlas
     else
-        return TEXTURE_ATLAS[]
+        atlas = TextureAtlas()
+        for c in '\u0000':'\u00ff' #make sure all ascii is mapped linearly
+            insert_glyph!(atlas, c, defaultfont())
+        end
+        to_cache(atlas) # cache it
+        return atlas
     end
 end
+
+function to_cache(atlas)
+    if !ispath(dirname(atlas))
+        mkdir(dirname(atlas))
+    end
+    open(_cache_path, "w") do io
+        dict = Dict(
+            n => getfield(atlas, n) for n in fieldnames(atlas)
+        )
+        dict[:images] = gpu_data(dict[:images])
+        serialize(io, dict)
+    end
+end
+
+function get_texture_atlas()
+    if isempty(_textute_atlas)
+        push!(_textute_atlas, cached_load()) # initialize only on demand
+    end
+    _textute_atlas[]
+end
+
 end
 
 function glyph_index!(atlas::TextureAtlas, c::Char, font)
-    if c < '\u00ff' && font == DEFAULT_FONT_FACE # characters up to '\u00ff'(255), are directly mapped for default font
+    if c < '\u00ff' && font == defaultfont() # characters up to '\u00ff'(255), are directly mapped for default font
         Int(c)+1
     else #others must be looked up, since they're inserted when used first
         return insert_glyph!(atlas, c, font)
     end
 end
 
-glyph_scale!(c::Char) = glyph_scale!(get_texture_atlas(), c, DEFAULT_FONT_FACE)
-glyph_uv_width!(c::Char) = glyph_uv_width!(get_texture_atlas(), c, DEFAULT_FONT_FACE)
+glyph_scale!(c::Char) = glyph_scale!(get_texture_atlas(), c, defaultfont())
+glyph_uv_width!(c::Char) = glyph_uv_width!(get_texture_atlas(), c, defaultfont())
 
 function glyph_uv_width!(atlas::TextureAtlas, c::Char, font)
     atlas.attributes[glyph_index!(atlas, c, font)]
